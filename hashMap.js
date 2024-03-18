ba14a1491d7a46270814542f314017f7f60aaf95
Brendan Freeman <bfreeman0@gmail.com>
2024-03-18 16:33:32 +0000

    Mar 18, 2024, 4:33 PM

--- hashMap.js
+++ hashMap.js
@@ -2,20 +2,17 @@ export default () => {
   const hash = (key) => {
     let hashCode = 0;
     const primeNumber = 31;
-    let aMap = []
+    let aMap = new Array(16)
     
     for (let i = 0; i < key.length; i++) {
       hashCode = primeNumber * hashCode + key.charCodeAt(i);
     }
+    console.log(`${key} will go in the ${hashCode % aMap.length} bucket`);
 
     return hashCode;
   }
 
-  const set = (key, value) => {
-    const hashValue = hash(key)
-    
-
-  }
+  const set = (key, value) => {}
   return {
     getHash: (key) => hash(key)
   }
