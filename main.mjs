0d115ef5de545972dc711ad403d121385a4bfd6e
Brendan Freeman <bfreeman0@gmail.com>
2024-03-24 15:09:55 +0000

    Almost done

--- main.mjs
+++ main.mjs
@@ -1,12 +1,26 @@
-import hash from './hashMap.mjs'
+import hash from "./hashMap.mjs";
 
 const checkValidIndex = (index, buckets) => {
-    if (index < 0 || index >= buckets.length) {
-        throw new Error("Trying to access index out of bound");
-    }
-}
+  if (index < 0 || index >= buckets.length) {
+    throw new Error("Trying to access index out of bound");
+  }
+};
 
+const theMap = hash();
 
-const theMap = hash()
-
-console.log(`The bucket for the word "admin" is ${theMap.getBucket("admin")}`)
+console.log(`The bucket for the word "admin" is ${theMap.getBucket("admin")}`);
+console.log(`The bucket for the word "minda" is ${theMap.getBucket("minda")}`);
+console.log(
+  `The bucket for the word "oliver" is ${theMap.getBucket("oliver")}`
+);
+console.log(`The bucket for the word "dad" is ${theMap.getBucket("dad")}`);
+console.log(`${theMap.set("admin", "Brendan")}`);
+console.log(`${theMap.set("admin", "Trees")}`);
+console.log(`${theMap.set("oliver", "son")}`);
+console.log(`${theMap.set("dad", "brendan")}`);
+console.log(theMap.getContents(7));
+console.log(`The value of "admin" is ${theMap.get("admin")}`);
+console.log(`Does theMap contain key if oliver: ${theMap.has("oliver")}`);
+console.log(`Delete Olive if exists: ${theMap.remove("Oliv")}`);
+console.log(`Delete oliver if exists: ${theMap.remove("oliver")}`);
+console.log(`Contents of hashmap: ${theMap.length()}`);
